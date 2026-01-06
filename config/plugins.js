3456e05fd936e5a80b34df606985b63b204d8c6e
Abid Rakhmansyah <bidnth@gmail.com>
2026-01-06 14:59:53 +0700

    fix: add some new plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,28 @@
-module.exports = () => ({});
+module.exports = () => ({
+  // CKEditor plugin
+  ckeditor: {
+    enabled: true,
+  },
+  // SEO plugin
+  seo: {
+    enabled: true,
+  },
+  // Comments plugin
+  comments: {
+    enabled: true,
+  },
+  // Webtools plugin
+  webtools: {
+    enabled: true,
+  },
+  // Sentry plugin
+  sentry: {
+    enabled: true,
+    // Add your Sentry DSN here when ready
+    // dsn: process.env.SENTRY_DSN || '',
+  },
+  // Navigation plugin
+  navigation: {
+    enabled: true,
+  },
+});
